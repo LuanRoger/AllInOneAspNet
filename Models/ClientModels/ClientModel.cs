31abd78dcfb14bedce59584ab456897acf2b67eb
LuanRoger <luan.roger.2003@gmail.com>
2023-04-23 09:50:11 -0300

    feat(models): map models to be used with Entity Framework

--- Models/ClientModels/ClientModel.cs
+++ Models/ClientModels/ClientModel.cs
@@ -1,8 +1,24 @@
-﻿namespace AllInOneAspNet.Models.ClientModels;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using AllInOneAspNet.Models.UserModels;
 
+namespace AllInOneAspNet.Models.ClientModels;
+
+[Table("Clients")]
 public class ClientModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Required]
+    [Column("ID")]
     public int id { get; set; }
+    
+    [Required]
+    [MaxLength(100)]
+    [Column("Username")]
     public string username { get; set; }
+    
+    [Required]
+    [Column("CreatedBy")]
     public UserModel createdBy { get; set; }
 }
--- Models/UserModels/UserModel.cs
+++ Models/UserModels/UserModel.cs
@@ -1,9 +1,25 @@
-﻿namespace AllInOneAspNet.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace AllInOneAspNet.Models.UserModels;
+
+[Table("Users")]
 public class UserModel
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Required]
+    [Column("ID")]
     public int id { get; set; }
+    
+    [Required]
+    [Column("Username")]
     public string username { get; set; }
+    
+    [Column("Email")]
     public string email { get; set; }
+    
+    [Required]
+    [Column("Password")]
     public string password { get; set; }
 }
