98fda6eb8770172b06a881045b40456510ca13bf
LuanRoger <luan.roger.2003@gmail.com>
2023-04-23 09:47:55 -0300

    feat(controllers): interface for controllers created

---
+++ Controllers/IClientController.cs
@@ -0,0 +1,35 @@
+﻿using AllInOneAspNet.Models.ClientModels;
+
+namespace AllInOneAspNet.Controllers;
+
+public interface IClientController
+{
+    /// <summary>
+    /// Cadastrar um novo cliente
+    /// </summary>
+    /// <param name="registerRequestModel">Requisição de cadastro do cliente</param>
+    /// <returns>O ID do novo cliente</returns>
+    Task<int> RegisterClient(ClientRegisterRequestModel registerRequestModel);
+    
+    /// <summary>
+    /// Recupera os clientes cadastrados por um usuário
+    /// </summary>
+    /// <param name="userId">ID do usuario</param>
+    /// <returns>Uma lista dos clientes cadastrados pelo usuario</returns>
+    Task<IReadOnlyList<ClientReadModel>> GetUserClients(int userId);
+    
+    /// <summary>
+    /// Atualiza os dados de um cliente
+    /// </summary>
+    /// <param name="updateRequest">Requisição de atualização com as novas informações do cliente</param>
+    /// <param name="clientId">ID do cliente que será atualizado</param>
+    /// <returns>O ID do cliente atualizado</returns>
+    Task<int> UpdateClient(ClientUpdateRequestModel updateRequest, int clientId);
+    
+    /// <summary>
+    /// Deleta um cliente
+    /// </summary>
+    /// <param name="clientId">ID do cliente que será deletado</param>
+    /// <returns>O ID do cliente deletado</returns>
+    Task<int> DeleteClient(int clientId);
+}
---
+++ Controllers/IUserController.cs
@@ -0,0 +1,20 @@
+﻿using AllInOneAspNet.Models.UserModels;
+
+namespace AllInOneAspNet.Controllers;
+
+public interface IUserController
+{
+    /// <summary>
+    /// Cadastra um novo usuário
+    /// </summary>
+    /// <param name="signinRequest">Requisição de cadastro do usuário</param>
+    /// <returns>Retorna o ID do novo usuário</returns>
+    Task<int> SigninUser(UserSigninRequestModel signinRequest);
+    
+    /// <summary>
+    /// Autentica um usuário
+    /// </summary>
+    /// <param name="loginRequest">Requisição de login do usuário</param>
+    /// <returns>Retorna um novo JWT para o usuário</returns>
+    Task<string> LoginUser(UserLoginRequestModel loginRequest);
+}
