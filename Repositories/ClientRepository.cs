3ddd7fb654af897c54c4fc075de1eec880c24397
LuanRoger <luan.roger.2003@gmail.com>
2023-04-25 09:35:04 -0300

    feat(repositories): implement repos

---
+++ Repositories/ClientRepository.cs
@@ -0,0 +1,30 @@
+﻿using AllInOneAspNet.Models.ClientModels;
+using AllInOneAspNet.Repositories.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllInOneAspNet.Repositories;
+
+public class ClientRepository : IClientRepository
+{
+    private DatabaseContext dbContext { get; }
+    
+    public ClientRepository(DatabaseContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public async Task RegisterClient(ClientModel client) =>
+        await dbContext.client.AddAsync(client);
+
+    public async Task<ClientModel?> GetClientById(int clientId) =>
+        await dbContext.client.FindAsync(clientId);
+
+    public async Task<IReadOnlyList<ClientModel>> GetUserRelatedClient(int userId) =>
+        await dbContext.client
+            .Where(client => client.createdBy.id == userId)
+            .OrderBy(client => client.id)
+            .ToListAsync();
+
+    public async Task<ClientModel> DeleteClient(ClientModel clientModel) =>
+        dbContext.client.Remove(clientModel).Entity;
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,26 @@
+﻿using AllInOneAspNet.Models.UserModels;
+using AllInOneAspNet.Repositories.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace AllInOneAspNet.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private DatabaseContext dbContext { get; }
+    
+    public UserRepository(DatabaseContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    
+    public async Task RegisterUser(UserModel user) => 
+        await dbContext.user.AddAsync(user);
+
+    public async Task<UserModel?> GetUserById(int userId) => 
+        await dbContext.user.FindAsync(userId);
+
+    public async Task<UserModel?> GetUserByUsernamePassword(string username, string password) =>
+        await dbContext.user
+            .FirstOrDefaultAsync(user => user.username == username && 
+                                         user.password == password);
+}
