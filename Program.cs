b94dbcda7753615f52a7f2344004518ddf82127b
LuanRoger <luan.roger.2003@gmail.com>
2023-04-25 09:36:49 -0300

    feat(logging): add Serilog as default logging provider

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 using AllInOneAspNet.Endpoints;
+using Serilog;
+using ILogger = Serilog.ILogger;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+ILogger logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
+builder.Logging.ClearProviders();
+builder.Logging.AddSerilog(logger);
+builder.Host.UseSerilog(logger);
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<ClientRepository>();
 WebApplication app = builder.Build();
 
 RouteGroupBuilder userGroup = app.MapGroup("user");
