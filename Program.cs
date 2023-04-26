452b7f09f5f5543ff71a2e8a6b809c20c1b87815
LuanRoger <luan.roger.2003@gmail.com>
2023-04-26 08:23:36 -0300

    feat(di): update DI container dependencies

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 using AllInOneAspNet.Endpoints;
+using AllInOneAspNet.Models.ClientModels;
+using AllInOneAspNet.Models.UserModels;
+using AllInOneAspNet.Repositories;
+using AllInOneAspNet.Repositories.Contexts;
+using AllInOneAspNet.Validators.ClientValidators;
+using AllInOneAspNet.Validators.UserValidators;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 using ILogger = Serilog.ILogger;
 
@@ -10,8 +18,20 @@ ILogger logger = new LoggerConfiguration()
 builder.Logging.ClearProviders();
 builder.Logging.AddSerilog(logger);
 builder.Host.UseSerilog(logger);
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    string connectionString = @"Data Source=AllInOneDatabase.db;Version=3;";
+    options.UseSqlite(connectionString);
+});
 builder.Services.AddScoped<UserRepository>();
 builder.Services.AddScoped<ClientRepository>();
+
+builder.Services.AddScoped<IValidator<UserSigninRequestModel>, UserSigninRequestValidator>();
+builder.Services.AddScoped<IValidator<UserLoginRequestModel>, UserLoginRequestValidator>();
+builder.Services.AddScoped<IValidator<ClientRegisterRequestModel>, ClientRegisterRequestValidator>();
+builder.Services.AddScoped<IValidator<ClientUpdateRequestModel>, ClientUpdateRequestValidator>();
+
 WebApplication app = builder.Build();
 
 RouteGroupBuilder userGroup = app.MapGroup("user");
