1de8059288d99e4aeaa882d2145fb0eab554db38
LuanRoger <luan.roger.2003@gmail.com>
2023-04-29 08:52:18 -0300

    feat(service): create JWT service

---
+++ Services/Jwt/JwtConsts.cs
@@ -0,0 +1,9 @@
+﻿namespace AllInOneAspNet.Services.Jwt;
+
+public static class JwtConsts
+{
+    public const string JWT_SIMETRIC_KEY_SHA256 = 
+        "6d12dce15aee4d33e6f4b3ed4902604bd01b99af9f400f931a2d043c52ee0ec3";
+    public const string CLAIM_ID = "ID";
+    public const string JWT_ISSUER = "AllInOneAspNet";
+}
---
+++ Services/Jwt/JwtService.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Claims;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AllInOneAspNet.Services.Jwt;
+
+public class JwtService
+{
+    private byte[] simetricKey { get; }
+    
+    public JwtService(byte[] simetricKey)
+    {
+        this.simetricKey = simetricKey;
+    }
+    
+    public string GenerateToken(int userId)
+    {
+        JsonWebTokenHandler handler = new();
+        string jwt = handler.CreateToken(new SecurityTokenDescriptor
+        {
+            Subject = new(new []
+            {
+                new Claim(JwtConsts.CLAIM_ID, userId.ToString())
+            }),
+            SigningCredentials = new(new SymmetricSecurityKey(simetricKey), 
+                SecurityAlgorithms.HmacSha256Signature),
+            Issuer = JwtConsts.JWT_ISSUER,
+            IssuedAt = DateTime.Now
+        });
+        
+        return jwt;
+    }
+}
